b3b87ab415fb27bb92158af2a9d1298fcf52705f
ahadhasanjion <niallhoran3176@gmail.com>
2022-11-25 18:53:16 +0600

    user

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASSWORD}@cluster0.k5l2a3d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const usersCollection = client.db('crabs').collection('user');
+
+
+app.post('/users', async(req, res) => {
+    const userInfo = req.body;
+    const user = await usersCollection.findOne(userInfo)
+    if(user){
+        res.send(userInfo)
+    }
+    const result = await usersCollection.insertOne(userInfo)
+    res.send(result)
+})
+
+
+
+
+
 app.get('/', async (req, res) => {
     res.send('CARBS SERVER IS RUNNING');
 })
-
 app.listen(port, () => console.log(`CARBS SERVER RUNNING ON ${port}}`));
