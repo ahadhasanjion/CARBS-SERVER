a0e46ad293c9b46ba677589d96527035520861a0
ahadhasanjion <niallhoran3176@gmail.com>
2022-11-26 23:14:28 +0600

    booking

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000;
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
@@ -11,11 +11,17 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASSWORD}@cluster0.k5l2a3d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k5l2a3d.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 const usersCollection = client.db('crabs').collection('user');
+const categoryCollection = client.db('carbs').collection('category');
+const productsCollection = client.db('carbs').collection('products');
+const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+
+
+
 
 
 app.post('/users', async(req, res) => {
@@ -27,10 +33,43 @@ app.post('/users', async(req, res) => {
     const result = await usersCollection.insertOne(userInfo)
     res.send(result)
 })
+app.get('/category', async(req, res) => {
+    const query = {};
+    const result = await categoryCollection.find(query).toArray();
+    res.send(result)
+})
+app.post('/products',  async (req, res) => {
+    const product = req.body;
+    console.log(product)
+    const result = await productsCollection.insertOne(product);
+    res.send(result);
+});
+app.get("/products", async (req, res) => {
+    let query = {};
+    if (req.query.id)
+      query = {
+        categoryId: req.query.id,
+      };
+    const cursor = productsCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+});
 
-
-
-
+app.get('/category/:id', async(req, res) => {
+    let query = {};
+    if (req.params.id)
+    query = {
+      categoryId: req.params.id,
+    };
+    const cursor = await productsCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result)
+})
+app.post('/bookings', async (req, res) => {
+    const booking = req.body;
+    const result = await bookingsCollection.insertOne(booking);
+    res.send(result);
+});
 
 app.get('/', async (req, res) => {
     res.send('CARBS SERVER IS RUNNING');
